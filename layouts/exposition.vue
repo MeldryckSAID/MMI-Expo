5cad56b0b7600346e228166dc47fddaef88d5387
Quentin Heinis <94958477+QuentinHeinis@users.noreply.github.com>
2024-01-07 15:44:45 +0100

    feat(exposition layout) : add back to home link + style

--- layouts/exposition.vue
+++ layouts/exposition.vue
@@ -1,6 +1,7 @@
 <template>
   <main class="layout__page -expo">
     <NuxtPage />
+    <NuxtLink to="/" class="layout__link">Retour à l'accueil</NuxtLink>
   </main>
 </template>
 
@@ -8,6 +9,20 @@
 .layout {
   &__page.-expo {
     margin: 0;
+  }
+  &__link{
+    position: absolute;
+    bottom: 1rem;
+    right: 1rem;
+    color: white;
+    text-decoration: none;
+    border: 2px solid white;
+    padding: .25rem .5rem;
+    transition: all .3s ease;
+    &:hover{
+      background: white;
+      color: black;
+    }
   }
 }
 </style>
