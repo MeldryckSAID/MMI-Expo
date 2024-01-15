3078dc79ab9c894e1b586e941c35889b1fd6ad7d
Quentin Heinis <94958477+QuentinHeinis@users.noreply.github.com>
2024-01-15 23:32:51 +0100

    feat(nuxt) : import font + add nuxtimg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&display=swap",
+        },
+      ],
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width,initial-scale=1" },
+        { hid: "description", name: "description", content: "" },
+      ],
+    },
+  },
   routeRules: {
     "/exposition": { ssr: false },
   },
-  modules: ["@nuxtjs/prismic"],
+  modules: ["@nuxtjs/prismic", "@nuxt/image"],
   prismic: {
     endpoint: process.env.NUXT_PRISMIC_ENDPOINT,
   },
