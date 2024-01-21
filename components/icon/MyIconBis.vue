c33beeaedb4db3a581f686c7802253c17dfeba48
Quentin Heinis <94958477+QuentinHeinis@users.noreply.github.com>
2024-01-21 21:31:31 +0100

    feat(iconBis) : add new icons

---
+++ components/icon/MyIconBis.vue
@@ -0,0 +1,47 @@
+<script setup>
+import Click from "./icons/MyClick.vue";
+import Arrow from "./icons/MyArrow.vue";
+import BottomLeft from "./icons/MyBottomLeft.vue";
+import TopRight from "./icons/MyTopRight.vue";
+import Losange from "./icons/MyLosange.vue";
+import LosangeBis from "./icons/MyLosangeBis.vue";
+
+const props = defineProps({
+  name: String,
+  position: String,
+});
+
+const getIcon = computed(() => {
+  switch (props.name) {
+    case "Arrow":
+      return Arrow;
+    case "BottomLeft":
+      return BottomLeft;
+    case "TopRight":
+      return TopRight;
+    case "Click":
+      return Click;
+    case "Losange":
+      return Losange;
+    case "LosangeBis":
+      return LosangeBis;
+  }
+});
+</script>
+
+<template>
+  <i :class="`iconBis -${position}`">
+    <component :is="getIcon" />
+  </i>
+</template>
+
+<style lang="scss">
+.iconBis {
+  display: flex;
+  width: 50px;
+  svg{
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
